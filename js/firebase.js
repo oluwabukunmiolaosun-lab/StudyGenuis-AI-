c306f71b2bc23e7d99fd148f79a33be9d7919326
oluwabukunmiolaosun-lab <oluwabukunmiolaosun@gmail.com>
2026-07-24 23:00:45 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,19 @@
 
+// Firebase SDK imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB6H-tQ2f6Z4NmSsta1i1-SGYuD4pAb6WU"
+  authDomain: "studygenuis-ai.firebaseapp.com",
+  projectId: "studygenuis-ai",
+  storageBucket: "studygenuis-ai.firebasestorage.app",
+  messagingSenderId: "803615615503",
+  appId: "1:803615615503:web:1478c0e2782719567e917a"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
