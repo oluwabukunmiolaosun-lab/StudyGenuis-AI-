01dc639978bb920f752c0e6a404a967ee530909e
oluwabukunmiolaosun-lab <oluwabukunmiolaosun@gmail.com>
2026-07-26 07:41:23 +0100

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -1,30 +1,55 @@
-import { auth } from "./firebase.js";
+import { auth, db } from "./firebase.js";
+
 import {
   createUserWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import {
+  doc,
+  setDoc,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const signupBtn = document.getElementById("signup-btn");
 
 signupBtn.addEventListener("click", async () => {
 
+    const fullname = document.getElementById("fullname").value.trim();
     const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value;
     const confirmPassword = document.getElementById("confirm-password").value;
 
-    if(password !== confirmPassword){
+    if (fullname === "") {
+        alert("Please enter your full name.");
+        return;
+    }
+
+    if (password !== confirmPassword) {
         alert("Passwords do not match.");
         return;
     }
 
-    try{
+    try {
 
-        await createUserWithEmailAndPassword(auth, email, password);
+        const userCredential = await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
+        const user = userCredential.user;
+
+        await setDoc(doc(db, "users", user.uid), {
+            name: fullname,
+            email: email,
+            createdAt: serverTimestamp()
+        });
 
         alert("Account created successfully!");
 
         window.location.href = "login.html";
 
-    }catch(error){
+    } catch (error) {
 
         alert(error.message);
 
