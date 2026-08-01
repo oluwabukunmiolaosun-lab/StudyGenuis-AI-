257a3dc5c59901be9ea134983c92faf10a491bbe
oluwabukunmiolaosun-lab <oluwabukunmiolaosun@gmail.com>
2026-08-01 14:31:45 +0100

    Update gemini.js

--- api/gemini.js
+++ api/gemini.js
@@ -7,26 +7,25 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_API_KEY}
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          contents: [
+  `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      contents: [
+        {
+          parts: [
             {
-              parts: [
-                {
-                  text: message,
-                },
-              ],
+              text: message,
             },
           ],
-        }),
-      }
-    );
-
+        },
+      ],
+    }),
+  }
+);
     const data = await response.json();
 
     const reply =
